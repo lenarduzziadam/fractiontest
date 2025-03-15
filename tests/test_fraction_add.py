4d883f2d5b1c8656fd533bd450a51687651abbbf
lenarduzziadam <lenarduzziadam@gmail.com>
2025-03-15 00:37:21 -0500

    added some potential tests for init and add

--- tests/test_fraction_add.py
+++ tests/test_fraction_add.py
@@ -1,4 +1,42 @@
 import unittest
 
+from fraction import *
 class TestFractionAdd(unittest.TestCase):
     """Tests for the __add__ method of Fraction class."""
+    def setUp(self):
+        """Set up common fractions for testing"""
+        self.f1 = Fraction(1, 2)
+        self.f2 = Fraction(1, 3)
+        self.f3 = Fraction(3, 4)
+        self.f4 = Fraction(-1, 4)
+        self.f5 = Fraction(0, 1)
+        
+    def test_add_same_denominator(self):
+        """Test addition of fractions with the same denominator"""
+        result = self.f3 + self.f4
+        self.assertEqual(str(result), "1/2", "3/4 + (-1/4) should be 1/2")
+        
+    def test_add_different_denominator(self):
+        """Test addition of fractions with different denominators"""
+        result = self.f1 + self.f2
+        self.assertEqual(str(result), "5/6", "1/2 + 1/3 should be 5/6")
+        
+    def test_add_negative_fractions(self):
+        """Test addition with a negative fraction"""
+        result = self.f2 + self.f4
+        self.assertEqual(str(result), "1/12", "1/3 + (-1/4) should be 1/12")
+        
+    def test_add_zero(self):
+        """Test addition with zero"""
+        result = self.f1 + self.f5
+        self.assertEqual(str(result), "1/2", "1/2 + 0 should be 1/2")
+        
+    def test_add_whole_number(self):
+        """Test addition with a whole number"""
+        result = self.f3 + Fraction(2)
+        self.assertEqual(str(result), "11/4", "3/4 + 2 should be 11/4")
+        
+    def test_add_resulting_in_whole_number(self):
+        """Test addition resulting in a whole number"""
+        result = self.f1 + self.f1
+        self.assertEqual(str(result), "1/1", "1/2 + 1/2 should be 1/1")
--- tests/test_fraction_init.py
+++ tests/test_fraction_init.py
@@ -1,4 +1,49 @@
 import unittest
+from fraction import Fraction
+
 
 class TestFractionInit(unittest.TestCase):
-    """Tests for the __init__ method of Fraction class."""
+    #Tests for the __init__ method of Fraction class.
+    
+    def test_init_with_two_integers(self):
+        #Test initialization with two integers
+        f = Fraction(2, 4)
+        self.assertEqual(str(f), "1/2", "Fraction(2, 4) should be reduced to 1/2")
+        
+    def test_init_with_one_integer(self):
+        #Test initialization with one integer
+        f = Fraction(5)
+        self.assertEqual(str(f), "5/1", "Fraction(5) should create 5/1")
+        
+    def test_init_with_negative_numbers(self):
+        #Test initialization with negative numbers
+        f1 = Fraction(-3, 6)
+        self.assertEqual(str(f1), "-1/2", "Fraction(-3, 6) should be reduced to -1/2")
+        
+        f2 = Fraction(3, -6)
+        self.assertEqual(str(f2), "-1/2", "Fraction(3, -6) should be reduced to -1/2")
+        
+        f3 = Fraction(-3, -6)
+        self.assertEqual(str(f3), "1/2", "Fraction(-3, -6) should be reduced to 1/2")
+        
+    def test_init_with_zero_numerator(self):
+        #Test initialization with zero numerator
+        f = Fraction(0, 5)
+        self.assertEqual(str(f), "0/1", "Fraction(0, 5) should be reduced to 0/1")
+        
+    def test_init_with_zero_denominator(self):
+        #Test initialization with zero denominator
+        with self.assertRaises(ZeroDivisionError, msg="Should raise ZeroDivisionError when denominator is 0"):
+            Fraction(5, 0)
+            
+    def test_init_with_float(self):
+        #Test initialization with a float should raise TypeError
+        with self.assertRaises(TypeError, msg="Should raise TypeError when numerator is a float"):
+            Fraction(2.4)
+            
+        with self.assertRaises(TypeError, msg="Should raise TypeError when denominator is a float"):
+            Fraction(1, 2.5)
+
+
+if __name__ == '__main__':
+    unittest.main()
